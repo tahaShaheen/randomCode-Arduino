344c8f4309ded23e3cdc1adde99964b35710e72d
tahaShaheen <taha.shaheen@live.com>
2019-02-12 12:35:49 +0500

    NodeMCU prints out IP it is connected to

---
+++ NodeMCU_testing/NodeMCU_testing.ino
@@ -0,0 +1,28 @@
+#include "ESP8266WiFi.h"
+
+// WiFi parameters to be configured
+const char* ssid = "TP-LINK";
+const char* password = "+922199261203";
+
+void setup(void)
+{ 
+  Serial.begin(9600);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+
+  // while wifi not connected yet, print '.'
+  // then after it connected, get out of the loop
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+  //print a new line, then print WiFi connected and the IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+void loop() {
+  // Nothing
+}
