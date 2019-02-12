0f271c4e8bc61cdd89b8056a8e6ec3b5ac7e5aec
tahaShaheen <taha.shaheen@live.com>
2019-02-12 12:42:52 +0500

    NodeMCU hosts webpage

--- NodeMCU_testing/NodeMCU_testing.ino
+++ NodeMCU_testing/NodeMCU_testing.ino
@@ -1,28 +1,99 @@
-#include "ESP8266WiFi.h"
-
-// WiFi parameters to be configured
+#include <ESP8266WiFi.h>
+ 
 const char* ssid = "TP-LINK";
 const char* password = "+922199261203";
-
-void setup(void)
-{ 
-  Serial.begin(9600);
-  // Connect to WiFi
+ 
+int ledPin = D4; // Using the builtin LED
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
   WiFi.begin(ssid, password);
-
-  // while wifi not connected yet, print '.'
-  // then after it connected, get out of the loop
+ 
   while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
-  //print a new line, then print WiFi connected and the IP address
   Serial.println("");
   Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
   // Print the IP address
-  Serial.println(WiFi.localIP());
-
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
 }
+ 
 void loop() {
-  // Nothing
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Led pin is now: ");
+ 
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+ 
 }
