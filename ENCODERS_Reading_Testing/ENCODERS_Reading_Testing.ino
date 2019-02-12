5b34c03e6bf3c0752d50a8d40c3d7edba1c05d1b
tahaShaheen <taha.shaheen@live.com>
2019-02-12 13:00:56 +0500

    Includes steps overall taken

--- ENCODERS_Reading_Testing/ENCODERS_Reading_Testing.ino
+++ ENCODERS_Reading_Testing/ENCODERS_Reading_Testing.ino
@@ -1,44 +1,34 @@
-/*
- * The encoders aren't working on high speeds.
- * Ab dekhtay hain kya hota hai.
-*/
+#define encoder_L_C1 50 // Base Motor # 1
+#define encoder_R_C1 51 // Base Motor # 2
 
-
-#define CHA 2
-#define CHB 3
-
-volatile int master_count2 = 0; // universal count
-volatile int master_count3 = 0; // universal count
-long int intflag;// interrupt status flag
-long int previousmillis;
+volatile double countLeft = 0;
+volatile double countRight = 0;
+volatile double stepsOverall = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(CHA, INPUT);
-  pinMode(CHB, INPUT);
-  previousmillis = millis();
-  attachInterrupt(0, flag2, RISING);
-  attachInterrupt(1, flag3, RISING);
-  // interrupt 0 digital pin 2 positive edge trigger
-}
+  Serial1.begin(9600);
+    
+  pinMode(encoder_L_C1, INPUT);
+  pinMode(encoder_R_C1, INPUT);
 
-void flag2() {
-  master_count2++;
-}
-
-void flag3() {
-  master_count3++;
+  attachInterrupt(digitalPinToInterrupt(encoder_L_C1), encoderLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoder_R_C1), encoderRight, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  intflag = millis();
-  if (intflag == previousmillis + 500) {
-    Serial.print("2=");
-    Serial.println(master_count2);
-    Serial.print("3=");
-    Serial.println(master_count3);
-    intflag = millis();
-    previousmillis = millis();
-  }
+  Serial1.print("countLeft: "); Serial1.println(countLeft);
+  Serial1.print("countRight: "); Serial1.println(countRight);
+  Serial1.print("stepsOverall: "); Serial1.println(stepsOverall);
+
+  delay(1000);
+
+}
+
+void encoderLeft() {
+  countLeft++;
+  stepsOverall = (countLeft + countRight) / 2;
+}
+void encoderRight() {
+  countRight++;
+  stepsOverall = (countLeft + countRight) / 2;
 }
